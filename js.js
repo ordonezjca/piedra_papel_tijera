c02a53dedf2730112e850117d333fb3aeeddea1a
ordonezjca <ordoneztovarjuan@gmail.com>
2022-08-03 11:50:49 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,34 @@
+var lista_maquina = ["piedra", "papel", "tijera"]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+var opcion_jugador = prompt("Cual es tu jugada? digita el numero de tu eleccion: 1) piedra  2) papel  3) tijera");
+var opcion_maquina = lista_maquina[getRandomInt(2)];
+if (opcion_jugador == 1){
+    opcion_jugador = "piedra"
+}else if (opcion_jugador == 2){
+    opcion_jugador = "papel"
+}
+else if (opcion_jugador == 3){
+    opcion_jugador = "tijera"
+}
+document.write(opcion_jugador + " - ")
+document.write(opcion_maquina  + " - ");
+
+if (opcion_jugador == opcion_maquina){
+    document.write("Es un empate");
+}else if (opcion_jugador === "piedra" && opcion_maquina === "papel"){
+    document.write("perdiste");
+}else if (opcion_jugador === "piedra" && opcion_maquina === "tijera"){
+    document.write("Ganaste");
+}else if (opcion_jugador === "papel" && opcion_maquina === "piedra"){
+    document.write("ganaste");
+}else if (opcion_jugador === "papel" && opcion_maquina === "tijera"){
+    document.write("Perdiste");
+}else if (opcion_jugador === "tijera" && opcion_maquina === "piedra"){
+    document.write("perdiste");
+}else if (opcion_jugador === "tijera" && opcion_maquina === "papel"){
+    document.write("Ganaste");
+}
